9035c8e4370141cb90ca302998a108665d062271
the-asind <84527186+the-asind@users.noreply.github.com>
2023-04-04 21:21:46 +0700

    add projects

---
+++ ClientProgram/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientProgram
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var serverIP = IPAddress.Loopback.ToString();
+            using var client = new TcpClient();
+            try
+            {
+                await ConnectToServerAsync(client, serverIP);
+
+                while (true)
+                {
+                    var message = GetMessageFromUser();
+                    await SendMessageAsync(client, message);
+
+                    var response = await ReceiveMessageAsync(client);
+                    DisplayServerResponse(response);
+                }
+            }
+            catch (Exception ex)
+            {
+                HandleException(ex);
+            }
+            finally
+            {
+                CloseConnection(client);
+            }
+
+            Console.ReadKey();
+        }
+
+        static async Task ConnectToServerAsync(TcpClient client, string serverIP)
+        {
+            await client.ConnectAsync(serverIP, 29000);
+        }
+
+        static string GetMessageFromUser()
+        {
+            Console.Write("Enter a message to send to the server: ");
+            return Console.ReadLine();
+        }
+
+        static async Task SendMessageAsync(TcpClient client, string message)
+        {
+            var data = Encoding.UTF8.GetBytes(message);
+            await client.GetStream().WriteAsync(data, 0, data.Length);
+        }
+
+        static async Task<string> ReceiveMessageAsync(TcpClient client)
+        {
+            var data = new byte[256];
+            var response = new StringBuilder();
+            var bytes = await client.GetStream().ReadAsync(data, 0, data.Length);
+            response.Append(Encoding.ASCII.GetString(data, 0, bytes));
+            return response.ToString();
+        }
+
+        static void DisplayServerResponse(string response)
+        {
+            Console.WriteLine("Server response: " + response);
+        }
+
+        static void CloseConnection(TcpClient client)
+        {
+            client.Close();
+        }
+
+        static void HandleException(Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+    }
+}
---
+++ ServerProgram/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerProgram
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                IPAddress localAddr = IPAddress.Parse(IPAddress.Loopback.ToString());
+                TcpListener server = new TcpListener(localAddr, 29000);
+                server.Start();
+                Console.WriteLine("Server started.");
+
+                while (true)
+                {
+                    TcpClient client = await server.AcceptTcpClientAsync();
+                    Console.WriteLine("Client connected.");
+
+                    try
+                    {
+                        _ = Task.Run(() => ReceiveMessagesAsync(client));
+                        _ = Task.Run(() => SendMessagesAsync(client));
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+                server.Stop();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.HelpLink);
+            }
+            Console.ReadKey();
+        }
+
+        static async Task ReceiveMessagesAsync(TcpClient client)
+        {
+            try
+            {
+                while (true)
+                {
+                    byte[] data = new byte[256];
+                    int bytes = await client.GetStream().ReadAsync(data, 0, data.Length);
+                    string message = Encoding.UTF8.GetString(data, 0, bytes);
+                    Console.WriteLine("Received message: " + message);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                CloseConnection(client);
+            }
+        }
+
+        static async Task SendMessagesAsync(TcpClient client)
+        {
+            try
+            {
+                while (true)
+                {
+                    Console.Write("Enter a message to send to the client: ");
+                    string message = Console.ReadLine();
+                    byte[] data = Encoding.UTF8.GetBytes(message);
+                    await client.GetStream().WriteAsync(data, 0, data.Length);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                CloseConnection(client);
+            }
+        }
+
+        static void CloseConnection(TcpClient client)
+        {
+            client.Close();
+            Console.WriteLine("Client disconnected.");
+        }
+    }
+}
